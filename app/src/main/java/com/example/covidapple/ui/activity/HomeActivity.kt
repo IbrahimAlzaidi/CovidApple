c8ceec5786778ea5d5d2f658f2045cbece150341
HIPPODATA\ahs <ahs@hippodata.net>
2021-08-18 14:54:08 +0200

    Create basic code plate for the app packaging,naming and inital code.

---
+++ app/src/main/java/com/example/covidapple/ui/activity/HomeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.covidapple.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covidapple.databinding.ActivityHomeBinding
+import com.example.covidapple.util.CsvParser
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        openFile()
+    }
+
+    private fun openFile(){
+        val inputStream = assets.open("country_vaccinations.csv")
+        val buffer = BufferedReader(InputStreamReader(inputStream))
+        val parser = CsvParser()
+        buffer.forEachLine {
+            val currentLine = parser.parse(it)
+        }
+    }
+}
