6eb0ef3a1f137ad2fb46786d94d61070a43c0fc5
Rafal A <rafal.akeel99@gmail.com>
2021-08-18 19:19:57 +0300

    initial commit

--- app/src/main/java/com/example/covidapple/HomeActivity.kt
+++ app/src/main/java/com/example/covidapple/HomeActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //set the style back to normal after activating splash screen
+        setTheme(R.style.Theme_CovidApple)
+
         setContentView(R.layout.activity_home)
 
         
