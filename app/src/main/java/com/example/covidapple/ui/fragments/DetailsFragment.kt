1886f106c23d6e4de3d558aa33fc5a1f2546bc4b
Aymn S.Al-Arrak <jucko_15@yahoo.com>
2021-08-22 01:50:02 +0300

    add read more animation in detail fragment

--- app/src/main/java/com/example/covidapple/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/covidapple/ui/fragments/DetailsFragment.kt
@@ -1,6 +1,10 @@
 package com.example.covidapple.ui.fragments
 
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.transition.TransitionManager
 import com.example.covidapple.databinding.FragmentDetailsBinding
 import com.example.covidapple.databinding.FragmentHomeBinding
 
@@ -11,10 +15,23 @@ class DetailsFragment:BaseFragment<FragmentDetailsBinding>() {
         get() = FragmentDetailsBinding::inflate
 
     override fun setup() {
-
+        addExpandAbilty(binding?.textView, binding?.readMoreSymptomps)
     }
 
     override fun addCallBack() {
+
+    }
+
+
+
+    private fun addExpandAbilty(detailsText : TextView?, readMeView : TextView?){
+
+       readMeView?.setOnClickListener {
+           val isExpand = detailsText?.maxLines == Int.MAX_VALUE
+            TransitionManager.beginDelayedTransition(binding?.root as ViewGroup)
+            detailsText?.maxLines = if (isExpand) 2 else Int.MAX_VALUE
+            readMeView?.text = if (isExpand) "Read more" else "Show less"
+       }
 
     }
 }
