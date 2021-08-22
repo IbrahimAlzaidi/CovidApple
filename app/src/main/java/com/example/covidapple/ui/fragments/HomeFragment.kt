2e9647e43edb76fd01d1f059df16c000b6625e73
Rafal A <rafal.akeel99@gmail.com>
2021-08-22 21:44:44 +0300

    add functionality to learn more button and fix horizontal scroll view

--- app/src/main/java/com/example/covidapple/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/covidapple/ui/fragments/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.covidapple.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import com.example.covidapple.databinding.FragmentHomeBinding
 
@@ -7,12 +9,20 @@ class HomeFragment:BaseFragment<FragmentHomeBinding>() {
     override val LOG_TAG: String = "Home Fragment"
     override val bindingInflater: (LayoutInflater) -> FragmentHomeBinding
         get() = FragmentHomeBinding::inflate
-
     override fun setup() {
 
     }
 
     override fun addCallBack() {
+        binding?.reedMeButton?.setOnClickListener {
+            learnMore()
+        }
+    }
 
+    private fun learnMore(){
+        var readMoreArticle:String = "https://www.hopkinsmedicine.org/health/conditions-and-diseases/coronavirus"
+        var intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(readMoreArticle)
+        startActivity(intent)
     }
 }
