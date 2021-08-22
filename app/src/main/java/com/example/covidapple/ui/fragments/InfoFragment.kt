c10d4c6660ec096ddd8b016ebda31bfbdbe78666
Rafal A <rafal.akeel99@gmail.com>
2021-08-22 18:47:14 +0300

    add functionality to buttons in info screen

--- app/src/main/java/com/example/covidapple/ui/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/covidapple/ui/fragments/InfoFragment.kt
@@ -1,5 +1,7 @@
 package com.example.covidapple.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import com.example.covidapple.databinding.FragmentHomeBinding
 import com.example.covidapple.databinding.FragmentInfoBinding
@@ -14,5 +16,31 @@ class InfoFragment:BaseFragment<FragmentInfoBinding>() {
     }
 
     override fun addCallBack() {
+        var url:String
+        binding?.buttonGithubRafal?.setOnClickListener{
+                url="https://github.com/Rafal-a"
+                gitHubAccount(url)
+        }
+        binding?.buttonGithubIbrahim?.setOnClickListener {
+            url = "https://github.com/IbrahimAlzaidi"
+            gitHubAccount(url)
+        }
+        binding?.buttonGithubAymn?.setOnClickListener {
+            url = "https://github.com/juck0"
+            gitHubAccount(url)
+        }
+        binding?.buttonGithubBasheer?.setOnClickListener {
+            url = "https://github.com/basheer574"
+            gitHubAccount(url)
+        }
+        binding?.buttonGithubHuda?.setOnClickListener {
+            url = "https://github.com/huda997"
+            gitHubAccount(url)
+        }
+    }
+    private fun gitHubAccount(url:String){
+        var intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
