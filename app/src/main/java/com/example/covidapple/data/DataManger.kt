e0a641c7baee8c1617e9997d873e986d10a8c3ff
HIPPODATA\ahs <ahs@hippodata.net>
2021-08-22 15:17:39 +0200

    Merging all Ui and Develop Branches. Every thing is working fine without any problems.

--- app/src/main/java/com/example/covidapple/data/DataManger.kt
+++ app/src/main/java/com/example/covidapple/data/DataManger.kt
@@ -7,22 +7,28 @@ object DataManger {
     private val vaccineList = mutableListOf<Vaccine>()
     fun addVaccine(vaccine: Vaccine) = vaccineList.add(vaccine)
 
-    fun searchCountry( country : String) = vaccineList.filter { it.country?.contains(country)== true }
-    fun searchCountryGroup( country : String) = vaccineList.groupBy { it.country }
 
     fun getTotalVaccinationForCountry(country: String?) = vaccineList.filter {
         it.country?.equals(country, ignoreCase = true) == true
     }.sumOf { it.dailyVaccinations ?: 0 }
 
+    fun getTotalVaccinatedForCountry(country: String?) = vaccineList.filter {
+        it.country?.equals(country, ignoreCase = true) == true
+    }.sumOf { it.peopleVaccinated ?: 0 }
+
     fun getTotalVaccinationForAllCountries() = vaccineList.groupBy {
         it.country
     }.keys.associateWith { getTotalVaccinationForCountry(it) }
+
+    fun getTotalVaccinatedForAllCountries() = vaccineList.groupBy {
+        it.country
+    }.keys.associateWith { getTotalVaccinatedForCountry(it) }.toList().sortedBy { (_,v) -> v  }.take(10).toMap()
 
     fun getVaccinationMapOfCountry(country: String) = vaccineList.filter {
         it.country?.equals(country, ignoreCase = true) == true
     }.associate {
         Pair(it.date, it.dailyVaccinations)
-    }
+    }.toList().sortedBy { (_,v) -> v }.toMap()
 
 
 }
--- app/src/main/java/com/example/covidapple/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/covidapple/ui/fragments/SearchFragment.kt
@@ -1,7 +1,12 @@
 package com.example.covidapple.ui.fragments
 
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.widget.SearchView
+import com.example.covidapple.data.DataManger
 import com.example.covidapple.databinding.FragmentSearchBinding
+import org.eazegraph.lib.models.PieModel
+import kotlin.random.Random
 
 class SearchFragment:BaseFragment<FragmentSearchBinding>() {
     override val LOG_TAG: String = "Search Fragment"
@@ -14,5 +19,28 @@ class SearchFragment:BaseFragment<FragmentSearchBinding>() {
 
     override fun addCallBack() {
 
+        binding!!.apply {
+            search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                //click search icon in keyboard
+                override fun onQueryTextSubmit(query: String) = search(query)
+                override fun onQueryTextChange(newText: String?) = false
+            })
+            search.queryHint = "Looking for Country, Type it's Name"
+        }
     }
+
+    private fun search(country: String): Boolean {
+        binding?.apply {
+             DataManger.getVaccinationMapOfCountry(country).forEach { (t, u) ->
+                binding?.pieChart?.addPieSlice(u?.toFloat()?.let {
+                    PieModel(t,
+                        it, Color.argb(255, Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255)))
+                })
+            }
+            binding?.pieChart?.startAnimation()
+
+        }
+        return false
+    }
+
 }
